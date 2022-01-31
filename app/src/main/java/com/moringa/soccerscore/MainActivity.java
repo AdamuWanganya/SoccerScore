f5214cb1314d01754e43918c3ae115a32137cfcd
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-31 11:34:11 +0300

    create a Toast message and onClickListener method

--- app/src/main/java/com/moringa/soccerscore/MainActivity.java
+++ app/src/main/java/com/moringa/soccerscore/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringa.soccerscore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mbtnFindLeague;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbtnFindLeague = (Button) findViewById(R.id.btnFindLeague);
+        mbtnFindLeague.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Welcome to the League!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
