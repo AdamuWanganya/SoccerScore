97620ebb328d14761d86ceb729a24c0f5552b9fc
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-31 11:44:10 +0300

    create intent on onClick method to navigate from MainActivity to Leagues

--- app/src/main/java/com/moringa/soccerscore/MainActivity.java
+++ app/src/main/java/com/moringa/soccerscore/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringa.soccerscore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mbtnFindLeague.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Welcome to the League!!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,Leagues.class);
+                startActivity(intent);
             }
         });
     }
