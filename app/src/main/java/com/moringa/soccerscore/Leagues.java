1dd1f48a34e2c9b73f8f83bda6c33f26568dd1c1
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-31 13:38:11 +0300

    Add ButterKnife dependencies on build.gradle

--- app/src/main/java/com/moringa/soccerscore/Leagues.java
+++ app/src/main/java/com/moringa/soccerscore/Leagues.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Leagues extends AppCompatActivity {
     private TextView mleagueTextView;
@@ -25,6 +29,14 @@ public class Leagues extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, leagues);
         mlistView1.setAdapter(adapter);
+
+        mlistView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String league = ((TextView)view).getText().toString();
+                Toast.makeText(Leagues.this, league, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Intent intent = getIntent();
         String league = intent.getStringExtra("league");
