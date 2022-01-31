7dba880ea81445864e25b4e3f91fc68155c6b245
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-31 11:38:54 +0300

    create Leagues activity

---
+++ app/src/main/java/com/moringa/soccerscore/Leagues.java
@@ -0,0 +1,14 @@
+package com.moringa.soccerscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Leagues extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leagues);
+    }
+}
